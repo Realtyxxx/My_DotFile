538ce9d0d5e2af5fef571751b4c3f179fc474b75
realtyxxx <841344563@qq.com>
2021-11-20 15:10:27 +0800

    env.sh

---
+++ env.sh
@@ -0,0 +1,19 @@
+#export PATH=/Users/realtyxxx_mac/bin:$PATH
+
+#agents
+export https_proxy=http://127.0.0.1:1087 http_proxy=http://127.0.0.1:1087 all_proxy=socks5://127.0.0.1:1086
+
+
+#alias
+alias ty="open -a typora"
+alias se="source ~/env.sh"
+#tmux alias
+alias tas='tmux attach-session -t'
+alias tns='tmux new -s'
+alias tml='tmux ls'
+alias tks='tmux kill-server'
+
+fortune | pokemonsay
+
+#tex
+export PATH=/Library/Tex/texbin:$PATH
